6447f191237b91bbef146069061a9d9b41d5b6c3
Miguel Vicario <miguelvicario96@icloud.com>
2020-08-27 17:29:16 -0500

    Primer Post

--- 5_MVC/api/index.js
+++ 5_MVC/api/index.js
@@ -8,6 +8,18 @@ api.use(express.json({ extended: true }));
 
 api.get('/', (req, res) => res.send('Hello World'));
 
+// CRUD Usuario
+const { User } = require('../models');
+
+api.post('/api/v1/users', async (req, res) => {
+  try {
+    const user = await new User(req.body).save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = {
   api,
   PORT,
--- 5_MVC/index.js
+++ 5_MVC/index.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+const mongoose = require('mongoose');
 const { api, PORT } = require('./api');
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+)
+  .then(() => console.log('Database Connected'))
+  .catch((err) => console.log('Error Connecting To Database', err));
 
 api.listen(PORT, () => console.log(`Listening On Port ${PORT}`));
